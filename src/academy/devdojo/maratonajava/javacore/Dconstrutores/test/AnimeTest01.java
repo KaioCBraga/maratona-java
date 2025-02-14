ffe664bd90542b5251a2cb0e9468956891e4824b
Kaio Braga <oficial_kaio@hotmail.com>
2025-02-13 22:57:32 -0300

    Adicionando aulas complementares sobre construtores, blocos de inicialização e modificadores estatico

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("KAIO", "TV", 223, "Ação", "GTV");
+        anime.imprime();
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    {
+        System.out.println("Dentro do bloco de inicializacao");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
