432eeb5373edc6adb8d5cba48bf19f8ba5b155b1
Kaio Braga <oficial_kaio@hotmail.com>
2025-03-26 21:17:51 -0300

    Classes Utilitarias 01

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/NumberFormatTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        Locale localePT = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getInstance();
+        nfa[0] = NumberFormat.getInstance(localePT);
+        nfa[0] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getInstance(localeJP);
+        double valor = 10_000.2130;
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+    }
+}
